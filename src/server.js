83f9c31a5dfc5a03dad1db02a44fa5747bdc7073
Karasaev Magomed <rostovmen95@gmail.com>
2026-05-08 12:10:33 +0300

    add movieRoutes.js file

--- src/server.js
+++ src/server.js
@@ -4,7 +4,11 @@ const app = express();
 const PORT = 5001;      
 
 
-app.get('/', (req, res) => {})
+app.get('/', (req, res) => {
+    res.json({message: "Hello world"});
+});
+
+
 
 const server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
